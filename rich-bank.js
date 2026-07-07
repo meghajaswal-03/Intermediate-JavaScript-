9827ba0d3ef131c096a249eec1b8d8f2eaa0c146
meghajaswal-03 <meghajaswal03@gmail.com>
2026-07-07 09:16:18 -0700

    Debugging Exercise

---
+++ rich-bank.js
@@ -0,0 +1,123 @@
+const accounts = [
+	{id: 1, owner: "Alice", balance: 500},
+	{id: 2, owner: "Bob", balance: 300}
+];
+
+function getAccountById (id)
+{
+	for (const account of accounts)
+	{
+		if (account.id === id)
+		{
+			return account;
+		}
+	}
+}
+
+function createAccount (newAccountId, newAccountOwner)
+{
+	if (typeof newAccountId !== "number" || !Number.isInteger(newAccountId) || newAccountId <= 0)
+	{
+		throw new Error("Invalid account ID. Please provide a positive value");
+	}
+	if  (typeof newAccountOwner !== "string" || newAccountOwner.trim() === "")
+	{
+		throw new Error("Invalid account owner. Please provide a non-empty string.");
+	}
+
+	const existingAccount = getAccountById(newAccountId);
+	if (existingAccount)
+	{
+		throw new Error("An account with this ID already exists.");
+	}
+	accounts.push(
+		{
+			id: newAccountId,
+			owner: newAccountOwner,
+			balance: 0
+		}
+	);
+}
+
+function depositMoney (accountId, amount)
+{
+	const account = getAccountById(accountId);
+
+	if (!account)
+	{
+		throw new Error("Account not found");
+	}
+
+	if (typeof amount !== "number" || !Number.isFinite(amount) || amount <= 0)
+	{
+		throw new Error("Invalid deposit amount. Please provide a positive finite number.");
+	}
+
+	account.balance += amount;
+}
+
+function withdrawMoney (accountId, amount)
+{
+	const account = getAccountById(accountId);
+
+	if (!account)
+	{
+		throw new Error("Account not found.");
+	}
+
+	if (!Number.isFinite(amount))
+	{
+		throw new Error("Invalid value for withdrawal amount: The amount must be a finite number.");
+	}
+	if (amount < 0)
+	{
+		throw new Error("Invalid withdrawal amount");
+	}
+
+	account.balance -= amount;
+}
+
+function transferMoney (fromAccountId, toAccountId, amount)
+{
+	const fromAccount = getAccountById(fromAccountId);
+	const toAccount = getAccountById(toAccountId);
+
+	if (!fromAccount)
+	{
+		throw new Error("Source account not found.");
+	}
+
+	if (!Number.isFinite(amount) || amount < 0)
+	{
+		throw new Error("Invalid value for transfer amount: The amount must be a positive finite number.");
+	}
+
+	toAccount.balance += amount;
+}
+
+/*
+Hints:
+
+getAccountById("1");
+
+createAccount(1, "Alice");
+createAccount("3", "Charlie");
+createAccount(-3, "Charlie");
+createAccount(3, ["Charlie"]);
+createAccount(3, "");
+createAccount(3, "  ");
+
+depositMoney(1, "300")
+depositMoney(1, -300)
+depositMoney(1, 0)
+depositMoney(1, Infinity)
+depositMoney(4, 100)
+
+withdrawMoney(1, -100)
+withdrawMoney(1, 0)
+withdrawMoney(1, 501)
+
+transferMoney(1, 4, 100)
+transferMoney(1, 2, 501);
+transferMoney(1, 2, 100);
+*/
