765c15f45ec38f675c9783e390e0e4eb5ef4876e
meghajaswal-03 <meghajaswal03@gmail.com>
2026-08-03 00:22:01 -0700

    Arrow Functions
    Practicing arrow functions 11.8

---
+++ ArrowFunction.js
@@ -0,0 +1,28 @@
+/* Task 1: No Parameters: Activate Hyperdrive */
+// TODO: Write an arrow function named `activateHyperdrive` with no parameters that print `"Hyperdrive activated!"` to the console. Call `activateHyperdrive` to test it.
+activateHyperdrive = () => {
+    console.log("Hyperdrive activated!");
+}
+activateHyperdrive();
+/* Task 2: Implicit Return: Scan for Lifeforms */
+// TODO: Create an arrow function named `scanForLife` that implicitly returns `"No lifeforms detected"` without using curly braces. Print the result of calling `scanForLife`.
+scanForLife = () => "No lifeforms detected";
+console.log(scanForLife());
+
+/* Task 3: Implicit Return with Objects: Log Coordinates */
+// TODO: Write an arrow function named `currentCoordinates` that returns an object with properties `x`, `y`, and `z`, representing coordinates in space. Use implicit return. Print the returned object from `currentCoordinates`.
+currentCoordinates = () => ({x:10, y:20, z:30});
+console.log(currentCoordinates());
+/* Task 4: Understanding `this`: Message from Home Base */
+// TODO: Inside an object named `spacecraft`, create a method named `receiveMessage` using arrow function syntax. This method should log `"Message received: "` followed by a message it receives as a parameter. Directly call `receiveMessage` within `spacecraft` and observe. Observe and explain the behavior of `this` in this context as a comment.
+/*
+ * Observations:
+ * TODO: Explain here.
+ */
+const spacecraft = {
+    receiveMessage: (message) => {
+        console.log("Message received: " + this.message);
+    }
+};
+spacecraft.receiveMessage("Hello from Earth!");
+// the message that is given is undefined when used "this" because arrow functions do not have their own "this" context. Instead, they inherit "this" from the surrounding scope.
